75a887447c61f7dc0cf3fe497d548a31ceeae3f6
Hez Ronningen <code@hez.ca>
2020-03-05 21:14:41 -0800

    add stream/task ver

--- lib/elixir_poker.ex
+++ lib/elixir_poker.ex
@@ -11,18 +11,30 @@ defmodule ElixirPoker do
     |> IO.read()
     |> String.split("\n")
     |> Enum.reject(&(String.length(&1) == 0))
-    |> Enum.each(&process_line/1)
+    |> Enum.map(&process_line/1)
+    |> Enum.map(&log_line/1)
+    |> Enum.each(&IO.puts/1)
   end
 
-  defp process_line(line) do
-    sorted =
+  def run_stream do
+    :stdio
+    |> IO.stream(:line)
+    |> Task.async_stream(fn line ->
       line
-      |> parse_hands()
-      |> sort_hands()
+      |> String.trim()
+      |> process_line()
+      |> log_line()
+    end)
+    |> Stream.map(&elem(&1, 1))
+    |> Enum.each(&IO.puts/1)
+  end
 
-    #sorted |> Enum.map(&Hand.rank_string/1) |> Enum.join(", ") |> IO.inspect
-    max = List.first(sorted)
-    IO.puts("#{line}, Winner: #{Hand.to_string(max)}, Rank: #{Hand.rank_string(max)}")
+  defp process_line(line), do: {line, parse_hands(line)}
+
+  defp log_line({line, hands}) do
+    # sorted |> Enum.map(&Hand.rank_string/1) |> Enum.join(", ") |> IO.inspect
+    max = Enum.max_by(hands, & &1, Hand)
+    "#{line}, Winner: #{Hand.to_string(max)}, Rank: #{Hand.rank_string(max)}"
   end
 
   defp parse_hands(hands) do
